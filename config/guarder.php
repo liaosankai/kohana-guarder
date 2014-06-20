34c1a417f566e33a7a6ab15f6c4f7d34fb068d0b
Liao San-Kai <liaosankai@gmail.com>
2014-06-20 10:08:46 +0800

    註解設定內容

--- config/guarder.php
+++ config/guarder.php
@@ -3,20 +3,21 @@
 defined('SYSPATH') or die('No direct script access.');
 
 return array(
-    'autoload' => TRUE,
-    // 定義角色
+	/*
+    // 定義角色(角色名稱也可以
     'roles' => array(
+		// 未登入訪客
+		'guest' => array(
+		
+		),
+        // 角色名稱
         'login' => array(
             'allow' => array(
-                'api/news/*',
-            //   'api/*/*'
+
             ),
             'deny' => array(
-                //  'get://api/room/index',
-                'post://api/news/*',
-                'put://api/news/*',
-                'get://api/news/index'
-            )
+
+            ),
         ),
         'admin' => array(
             '*'
@@ -24,18 +25,7 @@ return array(
     ),
     // 定義資源
     'resources' => array(
-        'get://home/index' => array(
-            'title' => '檢視',
-            'visible' => FALSE,
-        ),
-        'get://home/ooo' => array(
-            'title' => '檢視',
-            'visible' => FALSE,
-        ),
-        'get://home/vvv' => array(
-            'title' => '檢視',
-            'visible' => FALSE,
-        ),
-        'post://api/Album/*'
+
     ),
+	*/
 );
